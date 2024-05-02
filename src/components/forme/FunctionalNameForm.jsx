13310ff3b5b62447536e2139ee72779b22e1b85b
Marina Budanko <barbaric1marina@gmail.com>
2024-05-02 18:54:23 +0200

    checkbox ref

--- src/components/forme/FunctionalNameForm.jsx
+++ src/components/forme/FunctionalNameForm.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 export function FunctionalNameForm() {
   const [name, setName]= useState ("pero");
   const onInputChangeHandler = (event) => {
@@ -23,27 +23,37 @@ export function FunctionalNameForm() {
 }
 
 export function NameForm() {
-  const[name, setName]=useState("")
+  const[name2, setName]=useState("Pero2");
+  const [numberAllowed, setNumberAllowed] =useState(false)
+  const nameInputRef = useRef(null);
+  const checkBoxInputRef = useRef(null);
   const onSubmitHandler = (event) => {
     event.preventDefault()
-    const formElement = event.target
-    const nameValue = formElement.name.value
+    const nameValue = nameInputRef.current.value
+    const checkboxValue =checkBoxInputRef.current.checked
     setName(nameValue)
-
-  
+    setNumberAllowed(checkboxValue)
   
   
   };
   return <form onSubmit={onSubmitHandler}>
     <h3>On submit</h3>
     <label htmlFor="name">Ime</label>
-    <input 
+    <input
+      ref={nameInputRef} 
       name="name" 
       type="text"
     />
+    <label htmlFor="box">Dopusti brojeve</label>
+    <input
+    ref ={checkBoxInputRef}
+    name="box"
+    type="checkbox"
+    />
     <button type="submit">Pošalji</button>
     <pre>
-      Trenutna vrijednost:{name}
+      Trenutna vrijednost:{name2}
+      Brojevi dopušteni: {(numberAllowed ? "da": "ne")}
     </pre>
   </form>;
 }
