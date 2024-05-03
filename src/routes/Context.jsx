19894e2328715900ee0fdeff94d7bfc5acd2eaaf
Marina Budanko <barbaric1marina@gmail.com>
2024-05-03 10:53:11 +0200

    context change through all?

--- src/routes/Context.jsx
+++ src/routes/Context.jsx
@@ -1,8 +1,31 @@
+import { useState } from "react";
 import { ContextExample1, ContextExample2, ContextExample3, ContextExample4 } from "../components/ContextExample.jsx";
 import Header from "../components/Header.jsx";
 import { Komponenta1 } from "../components/PropDrilling.jsx";
 import { FancyTitle } from "../components/Titles.jsx";
 import { NameContextProvider } from "../context/useNameContextValue.jsx";
+
+export function ContextWithState(){
+  const [name, setName]=useState("perica");
+  return (
+    <div>
+      <input type="text"
+      value={name}
+      onChange={(event) => {setName(event.target.value);
+      }}
+      />
+      <NameContextProvider value={name}>
+      <ContextExample1/>
+      <br/>
+      <ContextExample2/>
+      <br/>
+      <ContextExample3/>
+      <br/>
+      <ContextExample4/>
+      </NameContextProvider>
+      
+    </div>);
+}
 
 export function Context() {
   return(
@@ -10,17 +33,7 @@ export function Context() {
       <Header />
       <FancyTitle title="Context Page" />
       <Komponenta1 />
-      <NameContextProvider value="Peter">
-      <ContextExample1/>
-      <br/>
-      <ContextExample2/>
-      </NameContextProvider>
-       <br/>
-       <NameContextProvider value="Jane">
-        <ContextExample3/>
-        <br/>
-        <ContextExample4/>
-       </NameContextProvider>
+      <ContextWithState/>
       
       
     </div>);
